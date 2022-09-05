953c87020710efa37e82907249c4003816b9092c
omrsangx <28844245+omrsangx@users.noreply.github.com>
2022-09-05 13:24:21 -0400

    Create service-work.js

---
+++ static/js/service-work.js
@@ -0,0 +1,34 @@
+// WPA Flash Card
+var wpaAppName = 'SV Flash Card';
+
+var wpaAppCacheFiles = [
+  '/',
+  '/index.html',
+  '/static/css/style.css',
+  '/static/js/javascript.js',
+  '/static/images/favicon-16x16.png',
+  '/static/images/favicon-32x32.png',
+  '/static/images/favicon-180x180.png',
+  '/static/images/apple-touch-icon.png',
+  '/static/images/favicon-192x192.png',
+  '/static/images/favicon-512x512.png',
+  '/static/images/favicon.ico'
+];
+
+// Starting the service work
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(wpaAppName).then(function(cache) {
+      return cache.addAll(wpaAppCacheFiles);
+    })
+  );
+});
+
+// Serving cache content and files
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
